81f2a83281c6ca2330c18fbb02112a73b93bbc47
Suraj <urusanonyms@gmail.com>
2022-11-26 13:14:14 +0545

    Update fb-chat-bot.py

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -672,7 +672,7 @@ cookies = {
     "fr": "0NxayJuewRHQ30OX3.AWVJwIYNh0Tt8AJv6kSwDamhkoM.BiMrVd.Iu.AAA.0.0.BiMtVZ.AWXMVaiHrpQ",
     "c_user": "100087919702715",
     "datr": "xasyYs51GC0Lq5H5lvXTl5zA",
-    "xs": "42%3AvGjOUmzliBDisg%3A2%3A1669385639%3A-1%3A-1"
+    "xs": "49%3AD0lqU6anTpzOZA%3A2%3A1669447636%3A-1%3A-1"
 }
 
 
